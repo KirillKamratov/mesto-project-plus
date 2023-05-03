3064cbeed68dd973ac3686627e80aa4a2265ae36
Kirill Kamratov <kirill.kamratov@yandex.ru>
2023-05-03 23:42:23 +0300

    v. 2.3

--- src/routes/cards.ts
+++ src/routes/cards.ts
@@ -6,13 +6,13 @@ import {
   likeCard,
   dislikeCard,
 } from '../controllers/cards';
-import { cardCreationValidation, idValidation, cardIdValidation } from '../middlewares/validation';
+import { cardCreationValidation, cardIdValidation } from '../middlewares/validation';
 
 const router = Router();
 router.get('/', getAllCards);
 router.post('/', cardCreationValidation, createCard);
 router.delete('/:cardId', cardIdValidation, deleteCard);
-router.put('/:cardId/likes', idValidation, likeCard);
-router.delete('/:cardId/likes', idValidation, dislikeCard);
+router.put('/:cardId/likes', cardIdValidation, likeCard);
+router.delete('/:cardId/likes', cardIdValidation, dislikeCard);
 
 export default router;
